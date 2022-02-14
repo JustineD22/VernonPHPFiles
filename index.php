3ab1ab476b3bb300dd9882a9e8028819dd05b3dc
Justine <98328323+JustineD22@users.noreply.github.com>
2022-02-14 16:37:57 +0100

    ecriture fonction

--- index.php
+++ index.php
@@ -3,13 +3,25 @@ spl_autoload_register(function ($className) {
     include './classes/' .$className .'.php';
 });
 
-file_put_contents('./files/toto.txt' , 'Ecriture dans fichier');
+$fichier = fopen('./files/ecriture.txt', 'c+b');
 
-$contenu = file_get_contents('./files/toto.txt');
-$contenu .= "\nLa suite du fichier";
-file_put_contents('./files/toto.txt', $contenu, FILE_APPEND);
+fwrite($fichier, "ligne de  comm");
 
-Log::write('erreur');
+fclose($fichier);
+
+
+ //Ajout Ecriture
+
+// file_put_contents('./files/toto.txt' , 'Ecriture dans fichier');
+
+// $contenu = file_get_contents('./files/toto.txt');
+// $contenu .= "\nLa suite du fichier";
+// file_put_contents('./files/toto.txt', $contenu, FILE_APPEND);
+
+// Log::write('erreur');
+
+
+//Lecture
 
 // $fichier = file('./files/corbeau.txt');
 
